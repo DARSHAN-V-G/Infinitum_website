afb7a52f1b4dbeb07540c86b6efdbde9eb145c56
DARSHAN-V-G <darshanguna22@gmail.com>
2025-02-13 01:28:17 +0530

    Added google OAuth

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { registerWithEmail, login ,logout} = require('../controllers/authcontroller');
-
+const { registerWithEmail, login ,logout,googleLogin,handleGoogleLogin} = require('../controllers/authcontroller');
 const router = express.Router();
 
 router.post('/register', registerWithEmail); //general registration
-router.post('/login', login); //logging in using roll no and password
+//router.post('/login', login); //logging in using roll no and password
 router.post('/logout',logout); //logging out
+router.post("/callback", googleLogin);
+router.get("/google",handleGoogleLogin);
+
 module.exports = router;
+
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const studentRoutes = require('./routes/studentRoutes')
 dotenv.config();
 console.log("Connected to supabase");
 const app = express();
-app.use(cors());
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
