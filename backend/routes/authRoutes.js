50e9e9e59d24e6ef5c8650b6dbce1cd91f0404f9
DARSHAN-V-G <darshanguna22@gmail.com>
2025-02-13 18:42:47 +0530

    SILLY MISTAKE

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerWithEmail, login ,logout,googleLogin,handleGoogleLogin} = require('../controllers/authcontroller');
+const { registerWithEmail, login ,logout,googleLogin,handleGoogleLogin} = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', registerWithEmail); //general registration
